c0de09b261b6fdf64cd689101db79dd2925a40ef
Platon <platon20081019@gmail.com>
2025-12-20 12:33:15 +0200

    feat: add initial App component with main layout, KyivClock, AnimatedTitle, About components, and social links.

--- main.dev/src/App.tsx
+++ main.dev/src/App.tsx
@@ -9,7 +9,7 @@ function App() {
       <AnimatedTitle />
       <main className="lg:p-16 max-w-5xl ">
         <About />
-        <div className="mt-10 mb-10 flex items-center justify-center">
+        <div className="mt-10 mb-10 gap-4 flex items-center justify-center">
           <div>
             <a href="https://t.me/quartzee" target="_blank" rel="noreferrer">
               telegram {}
@@ -19,6 +19,7 @@ function App() {
               href="https://github.com/payton-ggg"
               target="_blank"
               rel="noreferrer"
+              className="ml-2"
             >
               github
             </a>
